6c0c08a5fe4e1e0bdc7ec4620c8504b85ae8d5ab
Lopez <ivlopez.dev@gmail.com>
2023-03-22 01:13:13 -0700

    Add optimized image

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const port = 3000
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
+app.use(express.static('assets'))
 
 app.get('/', function(req, res){
     res.sendFile( `${__dirname}/index.html`);
